93f3f5ccd766e81e391beea862d1fddf4fb395c5
Vanessa  Narciso <vanessanarciso@MacBook-Pro-de-Vanessa-2.local>
2018-08-11 15:08:28 -0500

    update

--- Ejecicios conJS/js/bom.js
+++ Ejecicios conJS/js/bom.js
@@ -4,6 +4,15 @@
 function getBom(){
 	console.log(window.innerHeight); 
 	console.log(window.innerWidth); 
+	console.log(window.location);
 }
+function redirect(url){
+	window.location.href = url;
+}
+
+function abrirVentana(url){
+	window.open(url);
+}
+
 
 getBom();
